3d5b2dbadb86b744f0fe6f60bba6e983227e27c1
Basia <you@example.com>
2019-03-29 16:24:27 +0100

    projects sections

---
+++ app.js
@@ -0,0 +1,39 @@
+// $('.project_foto').hover(function() {
+//     $(this).addClass('fade-out');
+//     $(this).addClass('displayNone');
+//     // setTimeout(function(){ $(this).addClass('displayNone'); }, 2000);
+// });
+
+var sellText = `Sprzedajesz nieruchomość?
+Nie wiesz jaką cenę zaproponować?
+Opłaciłeś już kilka portali ale skontaktowało się z Tobą tylko wielu agentów i kilku niezainteresowanych kupujących?
+Nie wiesz jak wykonać profesjonalne zdjęcia?
+Zastanawiasz się jak przygotować dom/ mieszkanie/ działkę do prezentacji?
+Obawiasz się, że nieruchomość ma wady prawne?
+Masz działkę rolna powyżej 0,3ha i nie wiesz kto może ją kupić?
+Boisz się, że nie skompletujesz dokumentów potrzebnych do aktu notarialnego?
+Mieszkasz daleko od nieruchomości, nie wiesz kto może Ci pomóc w sprzedaży?
+Podpisałeś już umowy pośrednictwa sprzedaży ale agent się do Ciebie nie odzywa?
+
+Dobrze trafiłeś.
+Zapraszam do współpracy.
+Rozwiążę Twoje problemy.
+Przeprowadzę Cie komfortowo i bezpiecznie przez proces sprzedaży nieruchomości.
+
+Co mogę zaoferować?
+Nikt na Świecie nie sprzedaje tyle nieruchomości co RE/MAX a ja korzystam z doświadczenia oraz know-how tej firmy
+Agenci RE/MAX 85% „swoich” nieruchomości sprzedają od 2 tygodni do 3 miesięcy. Postaram się aby Twoja oferta tak samo szybko znalazła nabywcę
+przeprowadzę analizę rynku oraz ustalę cenę ofertową
+wykonam profesjonalne materiały marketingowe oraz zadbam o odpowiednią reklamę
+ustalę stan prawny nieruchomości, pomogę skompletować potrzebne dokumenty
+będę Cie na bieżąco informować o rezultatach moich działań
+przeprowadzę home staging oraz  profesjonalne prezentacje
+będę dbała abyś uzyskał najlepszą cenę oraz negocjowała dla Ciebie
+Przeprowadzę Cię bezpiecznie przez proces sprzedaży (spotkanie u notariusza, przekazanie pieniędzy)
+pomogę w przekazaniu lokalu
+polecę usługi dodatkowe (doradca podatkowy, firma przeprowadzkowa)
+
+Jeśli uważasz że nie potrzebujesz mojej pomocy, gdyż masz tylko kilka pytań zapraszam na darmowe konsultacje.
+`;
+
+$('.sell').text(sellText);
